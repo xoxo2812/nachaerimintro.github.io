5be20aa3f00aa503adf266f5e825a815fedd73f5
xoxo2812 <nachaelim560@gmail.com>
2023-09-19 18:01:03 +0900

    body contents 1차 수정.

--- js/ncr.js
+++ js/ncr.js
@@ -1,11 +1,7 @@
-$(document).ready(function(){
-    $(".logo1").on('mousewheel',function(e){ 
-        var wheel = e.originalEvent.wheelDelta; 
-        if(wheel>0){ 
-        $(".logo1").html("<!--나채림-->");
-    
-        } else { 
-        $(".logo1").html("<!--IT'sME-->");
-            } 
-    });
-})
+const toggles = document.querySelectorAll(".faq-toggle");
+
+toggles.forEach((toggle) => {
+  toggle.addEventListener("click", () => {
+    toggle.parentNode.classList.toggle("active");
+  });
+});
