e62a42ee1f58c5ba53345e099eda4950da701169
xoxo2812 <nachaelim560@gmail.com>
2023-09-21 18:04:03 +0900

    body contents 7차 수정.

--- js/ncr.js
+++ js/ncr.js
@@ -5,3 +5,44 @@ toggles.forEach((toggle) => {
     toggle.parentNode.classList.toggle("active");
   });
 });
+
+var $list = $('.mnu>li>a');
+var $listImg = $('.view>li');
+var $viewOpen = $('.viewOpen');
+var $viewClose = $('.viewClose');
+
+var nowIdx = 0;
+
+$list.on('click',function(event){
+  event.preventDefault();
+  nowIdx = $list.index(this);
+
+  $list.eq(nowIdx).parent().addClass('on').siblings().removeClass('on');
+
+  $listImg.fadeOut();
+  $listImg.eq(nowIdx).fadeIn();
+});
+
+
+$viewOpen.on('click',function(event){
+
+  event.preventDefault();
+  var src = $(this).attr('href');
+
+  $viewImg.find('a').css({
+    backgroundImage : 'url('+src+')'
+  });
+
+  $viewImg.parent().fadeIn();
+});
+
+$viewClose.on('click',function(event){
+  event.preventDefault();
+  $viewImg.scrollTop(0)
+  $view.fadeOut();
+});
+
+$view.on('click',function(){
+  $viewImg.scrollTop(0)
+  $view.fadeOut();
+});
